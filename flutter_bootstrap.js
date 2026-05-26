f4ddaa9529bdb1fbf259f173fdfa3907bc7042ba
Drakblod <150568911+Drakblod@users.noreply.github.com>
2026-05-26 20:26:44 +0000

    Deploying to gh-pages from @ Drakblod/HejAppFlutter@885aba0040dabde931c08fcda58d99f43d4f1122 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "503704129" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3857948806" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
